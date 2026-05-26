f1490fb0e42ab726a4fa7a8059f55d1178791f6f
Andrew19683 <p3orpheus71@gmail.com>
2026-05-26 10:12:52 +0300

    learn how to group test

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+import pytest
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -8,17 +9,30 @@ def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self):
+        self.product = ProductFactory(title = "Best book created by robot")
+        # создаем по апи
+        self.link = self.product.link
+        yield
+        # после этого ключевого слова начинается teardown
+        # выполнится после каждого теста в классе
+        # удаляем те данные, которые мы создали 
+        self.product.delete()
+        
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 def test_guest_cant_see_product_in_basket(browser):
     page = MainPage(browser, link)
